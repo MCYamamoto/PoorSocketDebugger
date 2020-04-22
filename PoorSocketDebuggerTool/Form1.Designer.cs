a4e7b1265bdf67c2b51cd632774c5b793725b6d0
MCYamamoto <yamamoto@moltocarina.co.jp>
2020-04-22 11:38:10 +0900

    TCPクライアントの接続、切断、送信を実装

---
+++ PoorSocketDebuggerTool/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace PoorSocketDebuggerTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(292, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "接続";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(726, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "送信";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(292, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "切断";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "192.168.1.1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(124, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "50000";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(124, 97);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(141, 22);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "192.168.1.2";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(124, 125);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(141, 22);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "60000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "送信元IP";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "送信元Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "送信先Port";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "送信先IP";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(124, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(141, 23);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.Text = "TCPクライアント";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "種別";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(457, 22);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(348, 22);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.Text = "c:\\test.bin";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(454, 4);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(193, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "送信データのバイナリファイルパス";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(803, 22);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(64, 22);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(15, 168);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(994, 334);
+            this.listBox1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 521);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ PoorSocketDebuggerTool/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace PoorSocketDebuggerTool
+{
+    public partial class Form1 : Form
+    {
+        System.Net.Sockets.TcpClient m_tcp;
+        System.Net.Sockets.NetworkStream m_ns;
+        Socket localSocket;
+
+        Thread m_thread;
+        bool m_ActiveRecv = true;
+
+        delegate void ListAddDelegate();
+
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strSrcIP = textBox1.Text;
+            int sSrcPort = Int32.Parse(textBox2.Text);
+            string strDstIP = textBox3.Text;
+            int sDstPort = Int32.Parse(textBox4.Text);
+
+            try
+            {
+                localSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                var local = new System.Net.IPEndPoint(IPAddress.Parse(strSrcIP), sSrcPort);
+                localSocket.Bind(local);
+
+                //TcpClientを作成し、サーバーと接続する
+                m_tcp = new System.Net.Sockets.TcpClient();
+                m_tcp.Client = localSocket;
+                m_tcp.Connect(strDstIP, sDstPort);
+                listBox1.Items.Add("接続成功");
+
+                //NetworkStreamを取得する
+                m_ns = m_tcp.GetStream();
+                //読み取り、書き込みのタイムアウトを10秒にする
+                m_ns.ReadTimeout = 1000;
+                m_ns.WriteTimeout = 1000;
+
+                m_thread = new Thread(new ThreadStart(() =>
+                {
+                    System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                    byte[] resBytes = new byte[65536];
+                    int resSize = 0;
+                    do
+                    {
+                        try
+                        {
+                            //データの一部を受信する
+                            resSize = m_ns.Read(resBytes, 0, resBytes.Length);
+                            //Readが0を返した時はサーバーが切断したと判断
+                            if (resSize == 0)
+                            {
+                                listBox1.Items.Add("サーバ切断");
+                                break;
+                            }
+                            //受信したデータを蓄積する
+                            ms.Write(resBytes, 0, resSize);
+                            //まだ読み取れるデータがあるか、データの最後が\nでない時は、
+                            // 受信を続ける
+                            Invoke(new ListAddDelegate(OutputLog));
+
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+                    } while (m_ActiveRecv);
+                    //受信したデータを文字列に変換
+                    ms.Close();
+                    m_ns.Close();
+                    m_ns = null;
+                    m_tcp.Close();
+                    m_tcp = null;
+                    localSocket.Close();
+                    localSocket = null;
+                }));
+
+                m_thread.Start();
+
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+            }
+            catch (Exception)
+            {
+                listBox1.Items.Add("接続失敗");
+                if(m_ns != null)
+                {
+                    m_ns.Close();
+                }
+                if (m_tcp != null)
+                {
+                    m_tcp.Close();
+                }
+                if(localSocket != null)
+                {
+                    localSocket.Close();
+                }
+            }
+        }
+
+        private void OutputLog()
+        {
+            listBox1.Items.Add("受信");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(textBox5.Text, FileMode.Open, FileAccess.Read);
+
+            int fileSize = (int)fs.Length; // ファイルのサイズ
+            byte[] buf = new byte[fileSize]; // データ格納用配列
+
+            int readSize; // Readメソッドで読み込んだバイト数
+            int remain = fileSize; // 読み込むべき残りのバイト数
+            int bufPos = 0; // データ格納用配列内の追加位置
+
+            while (remain > 0)
+            {
+                // 1024Bytesずつ読み込む
+                readSize = fs.Read(buf, bufPos, Math.Min(1024, remain));
+
+                bufPos += readSize;
+                remain -= readSize;
+            }
+            fs.Dispose();
+            m_ns.Write(buf, 0, fileSize);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            m_ActiveRecv = false;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            listBox1.Items.Add("切断");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //タイトルを設定する
+            ofd.Title = "開くファイルを選択してください";
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、選択されたファイル名を表示する
+                textBox5.Text = ofd.FileName;
+            }
+        }
+    }
+}
